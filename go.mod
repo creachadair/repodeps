2eb6cae77bcebd97e9bfc5b68b3ce5909f6a9caf
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-26 14:50:33 -0700

    Add concurrent execution, separate output.

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/creachadair/repodeps
 go 1.12
 
 require (
+	github.com/creachadair/atomicfile v0.2.0
+	github.com/creachadair/taskgroup v0.1.0
 	github.com/golang/protobuf v1.3.1
 	gopkg.in/src-d/go-billy-siva.v4 v4.5.1
 	gopkg.in/src-d/go-billy.v4 v4.3.0
--- repodeps.go
+++ repodeps.go
@@ -6,17 +6,31 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 
+	"github.com/creachadair/atomicfile"
 	"github.com/creachadair/repodeps/deps"
 	"github.com/creachadair/repodeps/local"
 	"github.com/creachadair/repodeps/siva"
+	"github.com/creachadair/taskgroup"
+	"github.com/golang/protobuf/proto"
 )
 
 var (
-	doSourceHash = flag.Bool("sourcehash", false, "Record the names and digests of source files")
+	doSourceHash  = flag.Bool("sourcehash", false, "Record the names and digests of source files")
+	doSeparateOut = flag.Bool("separate", false, "Write output to a file per input")
+	doBinary      = flag.Bool("binary", false, "Write output as binary rather than JSON")
+	concurrency   = flag.Int("concurrency", 32, "Maximum concurrent workers")
+
+	out = &struct {
+		sync.Mutex
+		io.Writer
+	}{Writer: os.Stdout}
 )
 
 func main() {
@@ -24,35 +38,63 @@ func main() {
 	if flag.NArg() == 0 {
 		log.Fatalf("Usage: %s <repo-dir> ...", filepath.Base(os.Args[0]))
 	}
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 	opts := &deps.Options{
 		HashSourceFiles: *doSourceHash,
 	}
+	defer cancel()
+
+	g, run := taskgroup.New(taskgroup.Trigger(cancel)).Limit(*concurrency)
 
 	// Each argument is either a directory path or a .siva file path.
 	// Currently only rooted siva files are supported.
+	start := time.Now()
 	for _, dir := range flag.Args() {
+		dir := dir
 		path, err := filepath.Abs(dir)
 		if err != nil {
 			log.Fatalf("Resolving path: %v", err)
 		}
-		log.Printf("Processing %q...", dir)
+		run(func() error {
+			log.Printf("Processing %q...", dir)
 
-		var repos []*deps.Repo
-		if filepath.Ext(path) == ".siva" {
-			repos, err = siva.Load(ctx, path, opts)
-		} else {
-			repos, err = local.Load(ctx, path, opts)
-		}
-		if err != nil {
-			log.Printf("Skipped %q:\n  %v", dir, err)
-			continue
-		}
-
-		for _, repo := range repos {
-			if err = json.NewEncoder(os.Stdout).Encode(repo); err != nil {
-				log.Fatalf("Writing JSON: %v", err)
+			var repos []*deps.Repo
+			if filepath.Ext(path) == ".siva" {
+				repos, err = siva.Load(ctx, path, opts)
+			} else {
+				repos, err = local.Load(ctx, path, opts)
 			}
-		}
+			if err != nil {
+				log.Printf("Skipped %q:\n  %v", dir, err)
+				return nil
+			}
+
+			return writeRepos(ctx, path, repos)
+		})
 	}
+	if err := g.Wait(); err != nil {
+		log.Fatalf("Analysis failed: %v", err)
+	}
+	log.Printf("Analysis complete for %d inputs [%v elapsed]", flag.NArg(), time.Since(start))
+}
+
+func writeRepos(ctx context.Context, path string, repos []*deps.Repo) error {
+	bits, err := encodeRepos(repos)
+	if err != nil {
+		return err
+	}
+	if *doSeparateOut {
+		return atomicfile.WriteData(path+".deps", bits, 0644)
+	}
+	out.Lock()
+	defer out.Unlock()
+	_, err = out.Write(bits)
+	return err
+}
+
+func encodeRepos(repos []*deps.Repo) ([]byte, error) {
+	if *doBinary {
+		return proto.Marshal(&deps.Deps{Repositories: repos})
+	}
+	return json.Marshal(repos)
 }
