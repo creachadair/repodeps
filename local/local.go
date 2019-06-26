48c45015555bd496e3c636b90531fcbaed38dd31
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-26 16:32:02 -0700

    Lightly document package local.

--- local/local.go
+++ local/local.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package local analyzes Go dependencies for local Git repositories.
 package local
 
 import (
