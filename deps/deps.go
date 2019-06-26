6876ea8fe27cd40aedbdcdb810ad691bc259d8c9
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-26 08:24:18 -0700

    Add a protobuf storage definition.
    Replace the explicit definitions with a protobuf, since the generated types
    were already in the same shape anyway.

--- deps/deps.go
+++ deps/deps.go
@@ -6,38 +6,12 @@ import (
 	"strings"
 )
 
+//go:generate protoc --go_out=. deps.proto
+
 // Options control the behaviour of the Load function. A nil *Options behaves
 // as a zero-valued Options struct.
 type Options struct {
 	HashSourceFiles bool // record source file digests
-}
-
-// A Repo records the Go package structure of a repository.
-type Repo struct {
-	From     string     `json:"from,omitempty"` // file or directory, cosmetic
-	Remotes  []*Remote  `json:"remotes,omitempty"`
-	Packages []*Package `json:"packages,omitempty"`
-}
-
-// A Package records the name and dependencies of a single package.
-type Package struct {
-	Name       string   `json:"name"`
-	ImportPath string   `json:"importPath"`
-	Imports    []string `json:"imports,omitempty"`
-	Source     []*File  `json:"source,omitempty"`
-}
-
-// A Remote records the name and URL of a Git remote.
-type Remote struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-}
-
-// A File records the name and content digest of a file.  The name of the file
-// is recorded as a path relative to the root of the repository.
-type File struct {
-	Name   string `json:"name"`
-	Digest []byte `json:"sha256"`
 }
 
 // Hash produces a SHA-256 digest of the contents of r.
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/creachadair/repodeps
 go 1.12
 
 require (
+	github.com/golang/protobuf v1.3.1
 	gopkg.in/src-d/go-billy-siva.v4 v4.5.1
 	gopkg.in/src-d/go-billy.v4 v4.3.0
 	gopkg.in/src-d/go-git.v4 v4.12.0
