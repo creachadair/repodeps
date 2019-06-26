1a1f0572f88a34926baee6634dab9002354bb5b4
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-26 00:59:29 -0700

    Add repodeps, to scan git repositories for dependencies.

---
+++ repodeps.go
@@ -0,0 +1,52 @@
+// Program repodeps scans the contents of a collection of GitHub repositories
+// and reports the names and dependencies of any Go packages defined inside.
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/creachadair/repodeps/deps"
+	"github.com/creachadair/repodeps/local"
+	"github.com/creachadair/repodeps/siva"
+)
+
+var (
+	doSourceHash = flag.Bool("sourcehash", false, "Record the names and digests of source files")
+)
+
+func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		log.Fatalf("Usage: %s <repo-dir> ...", filepath.Base(os.Args[0]))
+	}
+	ctx := context.Background()
+	for _, dir := range flag.Args() {
+		path, err := filepath.Abs(dir)
+		if err != nil {
+			log.Fatalf("Resolving path: %v", err)
+		}
+		log.Printf("Processing %q...", dir)
+
+		var repos []*deps.Repo
+		if filepath.Ext(path) == ".siva" {
+			repos, err = siva.Load(ctx, path, &deps.Options{HashSourceFiles: *doSourceHash})
+		} else {
+			repos, err = local.Load(ctx, path, &deps.Options{HashSourceFiles: *doSourceHash})
+		}
+		if err != nil {
+			log.Printf("Skipped %q:\n  %v", dir, err)
+			continue
+		}
+
+		for _, repo := range repos {
+			if err = json.NewEncoder(os.Stdout).Encode(repo); err != nil {
+				log.Fatalf("Writing JSON: %v", err)
+			}
+		}
+	}
+}
