1da32f82c3cef30da54d439ffeb31180f8eb347a
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-27 22:01:30 -0700

    graph: Add a Scan method to Storage.

--- graph/graph.go
+++ graph/graph.go
@@ -3,6 +3,7 @@ package graph
 
 import (
 	"context"
+	"errors"
 
 	"github.com/creachadair/repodeps/deps"
 	"github.com/golang/protobuf/proto"
@@ -40,6 +41,25 @@ func (g *Graph) Row(ctx context.Context, pkg string) (*Row, error) {
 	return &row, nil
 }
 
+// Scan calls f with each row in the graph having the specified prefix.
+// If f reports an error, scanning terminates. If the error is ErrStopScan Scan
+// returns nil; otherwise Scan returns the error from f.
+func (g *Graph) Scan(ctx context.Context, prefix string, f func(*Row) error) error {
+	err := g.st.Scan(ctx, prefix, func(key string) error {
+		row, err := g.Row(ctx, key)
+		if err != nil {
+			return err
+		} else if err := f(row); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err == ErrStopScan {
+		return nil
+	}
+	return err
+}
+
 // Imports returns the import paths if the direct dependencies of pkg.
 func (g *Graph) Imports(ctx context.Context, pkg string) ([]string, error) {
 	row, err := g.Row(ctx, pkg)
@@ -56,4 +76,12 @@ type Storage interface {
 
 	// Store marshals the data from value and stores it under key.
 	Store(ctx context.Context, key string, val proto.Message) error
+
+	// Scan calls f with each key having the specified prefix. If f reports an
+	// error that error is propagated to the caller of Scan.
+	Scan(ctx context.Context, prefix string, f func(string) error) error
 }
+
+// ErrStopScan is returned by the callback to Scan to signal that scanning
+// should terminate without error.
+var ErrStopScan = errors.New("stop scanning")
