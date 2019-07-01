3cafabf7cd3df0ae28026378f54f07c2851850bd
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-06-30 18:35:17 -0700

    Add a package comment for the siva package.

--- siva/siva.go
+++ siva/siva.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package siva analyzes Go dependences for rooted Git repositories packed into
+// siva archives by github.com/src-d/borges.
 package siva
 
 import (
