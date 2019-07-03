703d5f0fef67db5c1cd2cb63c6b8caca656ba44c
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-03 11:15:00 -0700

    checkrepo: Add the ability to clone and update repositories.

--- tools/checkrepo/checkrepo.go
+++ tools/checkrepo/checkrepo.go
@@ -20,48 +20,107 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"sync"
 
+	"bitbucket.org/creachadair/stringset"
+	"github.com/creachadair/repodeps/deps"
+	"github.com/creachadair/repodeps/local"
 	"github.com/creachadair/repodeps/tools"
 	"github.com/creachadair/taskgroup"
 )
 
 var (
-	storePath   = flag.String("store", "", "Storage path (required)")
+	pollDBPath  = flag.String("polldb", "", "Poll database path (required)")
+	storePath   = flag.String("store", "", "Storage database path (required with -update)")
+	cloneDir    = flag.String("clone-dir", "", `Location to store clones ("" uses $TMPDIR)`)
 	doReadStdin = flag.Bool("stdin", false, "Read repo URLs from stdin")
+	doClone     = flag.Bool("clone", false, "Clone updated repositories")
+	doUpdate    = flag.Bool("update", false, "Update cloned repositories (implies -clone)")
 	concurrency = flag.Int("concurrency", 16, "Number of concurrent workers")
 )
 
 func main() {
 	flag.Parse()
 
-	db, c, err := tools.OpenPollDB(*storePath, tools.ReadWrite)
+	// Check command-line flags.
+	if *storePath == "" && *doUpdate {
+		log.Fatal("You must specify a non-empty -store in order to -update")
+	} else if (*doClone || *doUpdate) && *cloneDir == "" {
+		tmp, err := ioutil.TempDir("", "checkrepo-")
+		if err != nil {
+			log.Fatalf("Creating temp directory: %v", err)
+		}
+		*cloneDir = tmp
+	}
+
+	// Open the poll state database.
+	db, c, err := tools.OpenPollDB(*pollDBPath, tools.ReadWrite)
 	if err != nil {
 		log.Fatalf("Opening poll database: %v", err)
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+
+	// Set up an updater; by default a no-op.
+	update, cleanup := updater(ctx)
+	defer func() {
+		if err := cleanup(); err != nil {
+			log.Fatalf("Closing graph: %v", err)
+		}
+	}()
+
 	g, run := taskgroup.New(taskgroup.Trigger(cancel)).Limit(*concurrency)
-	var omu sync.Mutex
+
+	var omu sync.Mutex // guards stdout, numUpdates
+	var numUpdates int
+	seen := stringset.New()
 	enc := json.NewEncoder(os.Stdout)
+
 	for url := range tools.Inputs(*doReadStdin) {
+		url := url
+		if seen.Contains(url) {
+			log.Printf("[skipped] duplicate URL %q", url)
+			continue
+		}
+		seen.Add(url)
 		run(func() error {
 			res, err := db.Check(ctx, url)
 			if err != nil {
 				log.Printf("[skipped] checking %q: %v", url, err)
-			} else {
-				omu.Lock()
-				enc.Encode(result{
-					Need: res.NeedsUpdate(),
-					Repo: res.URL,
-					Name: res.Name,
-					Hex:  res.Digest,
-				})
-				omu.Unlock()
+				return nil
 			}
+			out := &result{
+				Need: res.NeedsUpdate(),
+				Repo: res.URL,
+				Name: res.Name,
+				Hex:  res.Digest,
+			}
+			if *doClone || *doUpdate {
+				path := filepath.Join(*cloneDir, res.Digest)
+				if err := res.Clone(ctx, path); err != nil {
+					log.Printf("[skipped] cloning %q failed: %v", res.URL, err)
+					return nil
+				}
+				out.Clone = path
+				if *doUpdate && !*doClone {
+					defer os.RemoveAll(path) // clean up after update, if -clone is not set
+				}
+				n, err := update(path)
+				if err != nil {
+					log.Printf("[skipped] updating %q failed: %v", res.URL, err)
+					return nil
+				}
+				out.Pkgs = n
+			}
+			omu.Lock()
+			numUpdates += out.Pkgs
+			enc.Encode(out)
+			omu.Unlock()
 			return nil
 		})
 	}
@@ -71,11 +130,44 @@ func main() {
 	if err := c.Close(); err != nil {
 		log.Fatalf("Closing storage: %v", err)
 	}
+	if *doUpdate {
+		log.Printf("Updated %d packages total", numUpdates)
+	}
 }
 
 type result struct {
-	Need bool   `json:"needsUpdate"`
-	Repo string `json:"repository"`
-	Name string `json:"name"`
-	Hex  string `json:"hexDigest"`
+	Need  bool   `json:"update"`
+	Repo  string `json:"repository"`
+	Name  string `json:"name"`
+	Hex   string `json:"digest"`
+	Clone string `json:"clone,omitempty"`
+	Pkgs  int    `json:"packages"`
+}
+
+func updater(ctx context.Context) (func(path string) (int, error), func() error) {
+	if !*doUpdate {
+		return func(string) (int, error) { return 0, nil }, func() error { return nil }
+	}
+	g, c, err := tools.OpenGraph(*storePath, tools.ReadWrite)
+	if err != nil {
+		log.Fatalf("Opening graph: %v", err)
+	}
+	cleanup := func() error { return c.Close() }
+	return func(path string) (int, error) {
+		repos, err := local.Load(ctx, path, &deps.Options{
+			HashSourceFiles:   true,
+			UseImportComments: true,
+		})
+		if err != nil {
+			return 0, err
+		}
+		var added int
+		for _, repo := range repos {
+			if err := g.AddAll(ctx, repo); err != nil {
+				return 0, err
+			}
+			added += len(repo.Packages)
+		}
+		return added, nil
+	}, cleanup
 }
