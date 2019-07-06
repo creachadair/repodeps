2512d5e7b6a60564ec038ec24310bceb55b97a0e
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-05 20:36:53 -0700

    Add program rankdeps, to compute PageRank on dependencies.

---
+++ tools/rankdeps/rankdeps.go
@@ -0,0 +1,142 @@
+// Copyright 2019 Michael J. Fromberger. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Program rankdeps computes an iterative PageRank value for a graph.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"time"
+
+	"github.com/creachadair/repodeps/graph"
+	"github.com/creachadair/repodeps/tools"
+)
+
+var (
+	storePath   = flag.String("store", os.Getenv("REPODEPS_DB"), "Storage path (required)")
+	numIter     = flag.Int("iterations", 10, "Number of iterations")
+	dampFactor  = flag.Float64("damping", 0.85, "Damping factor (0..1)")
+	scaleFactor = flag.Float64("scale", 100, "Scaling factor for rank values")
+)
+
+func main() {
+	flag.Parse()
+	g, c, err := tools.OpenGraph(*storePath, tools.ReadOnly)
+	if err != nil {
+		log.Fatalf("Opening graph: %v", err)
+	}
+	defer c.Close()
+
+	// Load and initialize the link graph.
+	ctx := context.Background()
+	m := make(linkMap)
+	if err := g.Scan(ctx, "", func(row *graph.Row) error {
+		m[row.ImportPath] = &node{cur: 1, links: row.Directs}
+		return nil
+	}); err != nil {
+		log.Fatalf("Initializing link graph: %v", err)
+	}
+	if len(m) == 0 {
+		return // nothing to do
+	}
+	log.Printf("Loaded graph with %d nodes", len(m))
+
+	// Iterate the ranking algorithm.
+	log.Printf("Computing %d ranking iterations...", *numIter)
+	start := time.Now()
+	m.initialize()
+	for i := 1; i <= *numIter; i++ {
+		fmt.Fprint(os.Stderr, ".")
+		m.push()
+		m.advance()
+	}
+	fmt.Fprintln(os.Stderr)
+	log.Printf("Rank computation complete [%v elapsed]", time.Since(start))
+
+	// Order and scale the rankings.
+	nodes := make([]*node, len(m))
+	i := 0
+	for key, node := range m {
+		node.key = key
+		nodes[i] = node
+		i++
+	}
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].cur > nodes[j].cur
+	})
+	sf := float64(*scaleFactor) / nodes[0].cur
+	for _, node := range nodes {
+		v := node.cur * sf
+		fmt.Printf("%s\t%.2f\n", node.key, v)
+	}
+}
+
+type node struct {
+	cur, next float64
+	links     []string
+	key       string
+}
+
+type linkMap map[string]*node
+
+// initialize adds any links that do not have a row in the graph
+// to the link map, and normalizes each node's weight by the total
+// number of nodes.
+func (m linkMap) initialize() {
+	for _, elt := range m {
+		for _, link := range elt.links {
+			if _, ok := m[link]; !ok {
+				m[link] = &node{cur: 1} // no outbound links here
+			}
+		}
+	}
+	n := float64(len(m))
+	for _, elt := range m {
+		elt.cur /= n
+	}
+}
+
+// advance advances each node to its next value and applies the damping factor.
+// Precondition: next is the undamped sum of propagated weights from in-edges.
+// Postcondition: next == 0 at each node.
+func (m linkMap) advance() {
+	adj := 1 - *dampFactor
+	for _, elt := range m {
+		elt.cur = adj + *dampFactor*elt.next
+		elt.next = 0
+	}
+}
+
+// push pushes the current weight of each node to each of its targets.
+// Precondition: next == 0 for each node.
+func (m linkMap) push() {
+	for _, elt := range m {
+		if len(elt.links) == 0 {
+			continue // no links to push to
+
+			// In the random explorer model, this node should give its
+			// weight to all other nodes. Here we just assume that effect
+			// is negligible on a graph of any interesting size.
+		}
+		w := elt.cur / float64(len(elt.links))
+		for _, link := range elt.links {
+			m[link].next += w
+		}
+	}
+}
