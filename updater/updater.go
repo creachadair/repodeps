c36873be97eb5688ee45d2e511c78edd8f644845
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-10 11:49:18 -0700

    updater: Add a Remove method.

--- updater/updater.go
+++ updater/updater.go
@@ -284,6 +284,59 @@ type ScanRsp struct {
 	Elapsed time.Duration `json:"elapsed"` // time elapsed during the scan
 }
 
+// Remove removes package and repositories from the database.
+func (u *Updater) Remove(ctx context.Context, req *RemoveReq) (*RemoveRsp, error) {
+	pkgs := stringset.New()
+	for _, pkg := range req.Packages {
+		if err := u.graph.Remove(ctx, pkg); err == storage.ErrKeyNotFound {
+			continue
+		} else if err != nil {
+			u.pushLog(ctx, req.LogErrors, "log.removeError", struct {
+				P string `json:"package"`
+				M string `json:"message"`
+			}{P: pkg, M: err.Error()})
+		} else {
+			pkgs.Add(pkg)
+		}
+	}
+	repos := stringset.FromIndexed(len(req.Repositories), func(i int) string {
+		return tools.FixRepoURL(req.Repositories[i])
+	})
+	if len(repos) != 0 {
+		if err := u.graph.Scan(ctx, "", func(row *graph.Row) error {
+			if repos.Contains(row.Repository) {
+				err := u.graph.Remove(ctx, row.ImportPath)
+				if err != nil {
+					u.pushLog(ctx, req.LogErrors, "log.removeError", err)
+				} else {
+					pkgs.Add(row.ImportPath)
+				}
+				return nil
+			}
+			return nil
+		}); err != nil {
+			return nil, err
+		}
+	}
+	return &RemoveRsp{
+		Repositories: repos.Elements(),
+		Packages:     pkgs.Elements(),
+	}, nil
+}
+
+// RemoveReq is the request parameter to the Remove method.
+type RemoveReq struct {
+	Repositories []string `json:"repositories"`
+	Packages     []string `json:"packages"`
+	LogErrors    bool     `json:"logErrors"`
+}
+
+// RemoveRsp is the result from a successful Remove call.
+type RemoveRsp struct {
+	Repositories []string `json:"repositories,omitempty"` // repositories removed
+	Packages     []string `json:"packages,omitempty"`     // packages removed
+}
+
 func (u *Updater) cloneAndUpdate(ctx context.Context, res *poll.CheckResult, opts *deps.Options) (int, error) {
 	path, err := ioutil.TempDir(u.opts.WorkDir, res.Digest)
 	if err != nil {
@@ -314,6 +367,8 @@ func (u *Updater) pushLog(ctx context.Context, sel bool, key string, arg interfa
 		var data json.RawMessage
 		e.UnmarshalData(&data)
 		arg = []interface{}{e.Message(), data}
+	} else if e, ok := arg.(error); ok {
+		arg = e.Error()
 	}
 	jrpc2.ServerPush(ctx, key, arg)
 }
