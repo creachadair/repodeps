b70f9f49b786a78e69d4b8292a52578f388fdd21
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-10 16:21:49 -0700

    checkrepo: Replace implementation with use of package updater.

--- tools/checkrepo/checkrepo.go
+++ tools/checkrepo/checkrepo.go
@@ -21,31 +21,25 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"math/rand"
 	"os"
 	"path/filepath"
 	"strings"
-	"sync"
 	"time"
 
-	"bitbucket.org/creachadair/stringset"
 	"github.com/creachadair/repodeps/deps"
-	"github.com/creachadair/repodeps/local"
 	"github.com/creachadair/repodeps/tools"
-	"github.com/creachadair/taskgroup"
+	"github.com/creachadair/repodeps/updater"
 )
 
 var (
-	pollDBPath   = flag.String("polldb", os.Getenv("REPODEPS_POLLDB"), "Poll database path (required)")
-	storePath    = flag.String("store", "", "Storage database path (required with -update)")
+	repoDBPath   = flag.String("repo-db", os.Getenv("REPODEPS_POLLDB"), "Poll database path (required)")
+	graphDBPath  = flag.String("graph-db", "", "Storage database path (required with -update)")
 	cloneDir     = flag.String("clone-dir", "", `Location to store clones ("" uses $TMPDIR)`)
 	doForce      = flag.Bool("force", false, "Force update of matching repositories")
 	doReadStdin  = flag.Bool("stdin", false, "Read repo URLs from stdin")
 	doScanDB     = flag.Bool("scan", false, "Read repo URLs from the poll database")
-	doClone      = flag.Bool("clone", false, "Clone updated repositories")
-	doUpdate     = flag.Bool("update", false, "Update cloned repositories (implies -clone)")
+	doUpdate     = flag.Bool("update", false, "Update cloned repositories")
 	errorLimit   = flag.Int("error-limit", 10, "Discard repositories that fail more than this many times")
 	pollInterval = flag.Duration("interval", 1*time.Hour, "Minimum polling interval")
 	sampleRate   = flag.Float64("sample", 1, "Sample this fraction of eligible updates (0..1)")
@@ -60,12 +54,12 @@ func init() {
        %[1]s [options] -scan      -- process URLs from a dependency graph (requires -store)
 
 Scan the specified Git repository URLs to see whether any have changed since
-the last update recorded in the database. Use -polldb to specify the path of
+the last update recorded in the database. Use -repo-db to specify the path of
 the database, or set REPODEPS_POLLDB in the environment.
 
 By default %[1]s processes URLs named on the command line. Use -stdin to
 additionally read URLs from stdin, one per line. Use -scan to read URLs from an
-existing dependency graph database (-store).
+existing dependency graph database (-graph-db).
 
 Updates are scheduled not less than -interval apart, and scale to the frequency
 of observed changes in the repository. Of the eligible URLs, a random fraction
@@ -86,16 +80,11 @@ reported as needing an update.  If the errors count exceeds -error-limit, the
 repository is removed from the database. Otherwise, if the repository has
 changed since the last check it is reported as needing an update.
 
-If -clone is set, each repository reported as needing an update will be fetched
-and checked out at the new commit position in a subdirectory of -clone-dir.  In
-this case a "clone" field is added to the output naming the directory.
-
 If -update is set, each repository reported as needing an update will be
 fetched and checked out at the new commit position, scanned for new dependency
-information, and written to the specified -store. If -clone is also set, these
-clones are retained when the program exits; otherwise they are deleted once the
-update is complete. If -force is true, -update will update all eligible repos,
-even those which have not changed.
+information, and written to the specified -graph-db. These clones are deleted
+once the update is complete.  If -force is true, -update will update all
+eligible repos, even those which have not changed.
 
 Up to -concurrency repositories may be concurrently processed.
 
@@ -108,169 +97,66 @@ Options:
 func main() {
 	flag.Parse()
 
-	// Check command-line flags.
+	enc := json.NewEncoder(os.Stdout) // log writer
+	opts := updater.Options{
+		RepoDB:  *repoDBPath,
+		GraphDB: *graphDBPath,
+		WorkDir: *cloneDir,
+
+		// Scan options.
+		MinPollInterval: *pollInterval,
+		ErrorLimit:      *errorLimit,
+		SampleRate:      *sampleRate,
+		Concurrency:     *concurrency,
+
+		// Default package loader options.
+		Options: deps.Options{
+			HashSourceFiles:   true,
+			UseImportComments: true,
+		},
+
+		// Stream logs to stdout.
+		StreamLog: func(_ context.Context, key string, arg interface{}) error {
+			return enc.Encode([]interface{}{time.Now().In(time.UTC), key, arg})
+		},
+	}
 	*logFilter = strings.ToUpper(*logFilter)
-	if *storePath == "" && *doUpdate {
-		log.Fatal("You must specify a non-empty -store in order to -update")
-	} else if *sampleRate < 0 || *sampleRate > 1 {
-		log.Fatalf("Sample rate %f is out of range (0..1)", *sampleRate)
-	} else if (*doClone || *doUpdate) && *cloneDir == "" {
-		tmp, err := ioutil.TempDir("", "checkrepo-")
-		if err != nil {
-			log.Fatalf("Creating temp directory: %v", err)
-		}
-		defer os.Remove(tmp) // best-effort cleanup if empty at exit
-		*cloneDir = tmp
-	}
 
-	// Open the poll state database.
-	db, c, err := tools.OpenPollDB(*pollDBPath, tools.ReadWrite)
+	u, err := updater.New(opts)
 	if err != nil {
-		log.Fatalf("Opening poll database: %v", err)
+		log.Fatalf("Creating updater: %v", err)
 	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var urls <-chan string
-	if *doScanDB {
-		urls = tools.ScanDB(ctx, db, *pollInterval)
-	} else {
-		urls = tools.Inputs(*doReadStdin)
-	}
-
-	// Set up an updater; by default a no-op.
-	update, cleanup := updater(ctx)
 	defer func() {
-		if err := cleanup(); err != nil {
-			log.Fatalf("Closing graph: %v", err)
+		if err := u.Close(); err != nil {
+			log.Fatalf("Closing updater: %v", err)
 		}
 	}()
 
-	g, run := taskgroup.New(taskgroup.Trigger(cancel)).Limit(*concurrency)
-
-	var omu sync.Mutex // guards stdout, numUpdates
-	var numURL, numSamples, numUpdates, numDups int
-	seen := stringset.New()
-	enc := json.NewEncoder(os.Stdout)
-
-	start := time.Now()
-	for url := range urls {
-		numURL++
-		url := tools.FixRepoURL(url)
-		if seen.Contains(url) {
-			numDups++
-			continue
-		} else if !pickSample(url) {
-			continue
-		}
-		numSamples++
-		seen.Add(url)
-		run(func() error {
-			res, err := db.Check(ctx, url)
-			if err != nil && res == nil { // structural failure
-				log.Printf("[skipped] checking %q: %v", url, err)
-				return nil
-			}
-			if res.Errors > *errorLimit { // update failure
-				db.Remove(ctx, url)
-				log.Printf("Removed %q after %d failures", url, res.Errors)
-			}
-			out := &result{
-				Need: res.NeedsUpdate(),
-				Repo: res.URL,
-				Ref:  res.Name,
-				Hex:  res.Digest,
-				Errs: res.Errors,
-			}
-			if (res.NeedsUpdate() || *doForce) && (*doClone || *doUpdate) {
-				path := filepath.Join(*cloneDir, fmt.Sprintf("%s.%p", res.Digest, res))
-				if err := res.Clone(ctx, path); err != nil {
-					log.Printf("[skipped] cloning %q failed: %v", res.URL, err)
-					return nil
-				}
-				if *doUpdate && !*doClone {
-					defer os.RemoveAll(path) // clean up after update, if -clone is not set
-				} else {
-					out.Clone = path
-				}
-				n, err := update(path)
-				if err != nil {
-					log.Printf("[skipped] updating %q failed: %v", res.URL, err)
-					return nil
-				}
-				out.Pkgs = n
-			}
-			numUpdates += out.Pkgs
-			if pickLog(out) {
-				omu.Lock()
-				enc.Encode(out)
-				omu.Unlock()
-			}
-			return nil
-		})
-	}
-	if err := g.Wait(); err != nil {
-		log.Fatalf("Processing failed: %v", err)
-	}
-	if err := c.Close(); err != nil {
-		log.Fatalf("Closing storage: %v", err)
-	}
-	log.Printf(`Processing complete:
-%-6d URLs scanned
-%-6d duplicates discarded
-%-6d samples selected
-%-6d packages updated
-
-[%v elapsed]
-`, numURL, numDups, numSamples, numUpdates, time.Since(start).Truncate(time.Millisecond))
-}
-
-type result struct {
-	Need  bool   `json:"needsUpdate"`
-	Repo  string `json:"repository"`
-	Ref   string `json:"reference"`
-	Hex   string `json:"digest,omitempty"`
-	Clone string `json:"clone,omitempty"`
-	Pkgs  int    `json:"numPackages,omitempty"`
-	Errs  int    `json:"errors,omitempty"`
-}
-
-func updater(ctx context.Context) (func(path string) (int, error), func() error) {
-	if !*doUpdate {
-		return func(string) (int, error) { return 0, nil }, func() error { return nil }
-	}
-	g, c, err := tools.OpenGraph(*storePath, tools.ReadWrite)
-	if err != nil {
-		log.Fatalf("Opening graph: %v", err)
-	}
-	cleanup := func() error { return c.Close() }
-	return func(path string) (int, error) {
-		repos, err := local.Load(ctx, path, &deps.Options{
-			HashSourceFiles:   true,
-			UseImportComments: true,
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	if *doScanDB {
+		rsp, err := u.Scan(ctx, &updater.ScanReq{
+			LogUpdates:    strings.IndexByte(*logFilter, 'U') < 0,
+			LogErrors:     strings.IndexByte(*logFilter, 'E') < 0,
+			LogNonUpdates: strings.IndexByte(*logFilter, 'N') < 0,
 		})
 		if err != nil {
-			return 0, err
+			log.Printf("Scan failed: %v", err)
+		} else {
+			enc.Encode(rsp)
 		}
-		var added int
-		for _, repo := range repos {
-			if err := g.AddAll(ctx, repo); err != nil {
-				return 0, err
-			}
-			added += len(repo.Packages)
-		}
-		return added, nil
-	}, cleanup
-}
-
-func pickSample(_ string) bool { return rand.Float64() < *sampleRate }
-
-func pickLog(msg *result) bool {
-	if msg.Errs != 0 {
-		return strings.IndexByte(*logFilter, 'E') < 0
-	} else if msg.Need {
-		return strings.IndexByte(*logFilter, 'U') < 0
+		return
 	}
-	return strings.IndexByte(*logFilter, 'N') < 0
+	for url := range tools.Inputs(*doReadStdin) {
+		rsp, err := u.Update(ctx, &updater.UpdateReq{
+			Repository: url,
+			CheckOnly:  !*doUpdate,
+			Force:      *doForce,
+		})
+		if err != nil {
+			log.Printf("Update %q failed: %v", url, err)
+		} else {
+			enc.Encode(rsp)
+		}
+	}
 }
