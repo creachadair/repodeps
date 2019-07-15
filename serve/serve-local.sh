925e72cea8695e08f8f42955a4f2c33bc20be7f7
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-15 10:15:28 -0700

    Add serve-local.sh, to run a local instance not using Docker.
    At least on macOS, not interposing docker makes disk accesses on the order of
    8x faster, particularly at startup. There appears to be some kind of implicit
    copy step into the VM.

---
+++ serve/serve-local.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Usage: serve-local.sh
+#
+readonly port=9735
+readonly root="$HOME/software/sourced/data"
+readonly data='la-experiments:/mnt/data/repodeps'
+
+update() {
+  local base="${1:?missing database name}"
+  if [[ -f "${base}.snap" ]] ; then
+    badger restore -f "${base}.snap" --dir "${base}.new"
+    mv "$base" "${base}.old"
+    mv "${base}.new" "${base}"
+    rm -fr "${base}.old"
+  fi
+}
+
+set -e
+set -x
+pid="$(lsof -Fp -i4tcp:${port} | grep ^p | cut -c2-)"
+if [[ "$pid" != "" ]] ; then
+    kill -INT "$pid"
+    sleep 2
+fi
+
+set -o pipefail
+cd "$root"
+rsync -vzt -e 'ssh -o ClearAllForwardings=yes' "${data}/*.snap" .
+update graph-db
+update repo-db
+sync
+depserver -address "localhost:$port" \
+	  -graph-db graph-db \
+	  -repo-db repo-db \
+	  -rank-iter 40 \
+	  -rank-scale 6 \
+	  -sample-rate 0.1 &
