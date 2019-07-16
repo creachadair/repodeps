87900108019e0595780f6bc88826a676e5f0bf54
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-15 23:12:34 -0700

    Snapshot files need love too.

--- docker/serve-backup.sh
+++ docker/serve-backup.sh
@@ -8,8 +8,8 @@ set -e
 set -x
 docker stop deps-server
 (cd "$root" &&
-     sudo chown -R "$me" graph-db &&
-     sudo chown -R "$me" repo-db &&
+     sudo chown -R "$me" graph-db graph-db.snap &&
+     sudo chown -R "$me" repo-db repo-db.snap &&
      badger backup -f graph-db.snap --dir graph-db &&
      badger backup -f repo-db.snap --dir repo-db)
 ./serve-docker.sh reset
