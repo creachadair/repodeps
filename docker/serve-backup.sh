dbe58b8314086d45e4418ebde5d9a53b68aecdaa
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-15 23:10:02 -0700

    Fix permissions when backing up.
    Docker runs everything as root inside the container, and it's a pain to figure
    out which user to pretend to be. So just fix up the permissions during the
    snapshot and let God sort it out.

--- docker/serve-backup.sh
+++ docker/serve-backup.sh
@@ -2,11 +2,14 @@
 
 cd "$(dirname $0)"
 . config.sh
+readonly me="$(id -un):$(id -gn)"
 
 set -e
 set -x
 docker stop deps-server
 (cd "$root" &&
+     sudo chown -R "$me" graph-db &&
+     sudo chown -R "$me" repo-db &&
      badger backup -f graph-db.snap --dir graph-db &&
      badger backup -f repo-db.snap --dir repo-db)
 ./serve-docker.sh reset
