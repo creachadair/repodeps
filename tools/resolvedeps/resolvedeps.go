89761e327a519924d5300eefc8e41d6816d28578
M. J. Fromberger <michael.j.fromberger@gmail.com>
2019-07-21 12:39:47 -0700

    Resolve concurrently.

--- tools/resolvedeps/resolvedeps.go
+++ tools/resolvedeps/resolvedeps.go
@@ -22,32 +22,39 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
 
 	"bitbucket.org/creachadair/stringset"
 	"github.com/creachadair/jrpc2/code"
 	"github.com/creachadair/repodeps/client"
 	"github.com/creachadair/repodeps/service"
 	"github.com/creachadair/repodeps/tools"
+	"github.com/creachadair/taskgroup"
 )
 
 var (
 	address = flag.String("address", os.Getenv("REPODEPS_ADDR"), "Service address")
 
 	doReadStdin = flag.Bool("stdin", false, "Read package names from stdin")
+	numWorkers  = flag.Int("workers", 16, "Numcer of concurrent workers")
 )
 
 func main() {
 	flag.Parse()
 
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 	c, err := client.Dial(ctx, *address)
 	if err != nil {
 		log.Fatalf("Dialing service: %v", err)
 	}
 	defer c.Close()
 
+	var mu sync.RWMutex
 	pfxs := stringset.New()
 	seen := func(pkg string) bool {
+		mu.RLock()
+		defer mu.RUnlock()
 		for pfx := range pfxs {
 			if strings.HasPrefix(pkg, pfx) {
 				return true
@@ -55,24 +62,42 @@ func main() {
 		}
 		return false
 	}
+	addPfx := func(pfx string) {
+		mu.Lock()
+		defer mu.Unlock()
+		pfxs.Add(pfx)
+	}
 	repo := stringset.New()
+	addRepo := func(url string) {
+		mu.Lock()
+		defer mu.Unlock()
+		repo.Add(url)
+	}
+	g, run := taskgroup.New(taskgroup.Trigger(cancel)).Limit(*numWorkers)
 
 	for pkg := range tools.Inputs(*doReadStdin) {
-		if seen(pkg) {
-			continue
-		}
-		rsp, err := c.Resolve(ctx, pkg)
-		if err != nil {
-			log.Printf("Resolving %q: %v", pkg, err)
-			continue
-		}
-		pfxs.Add(rsp.Prefix)
-		_, err = c.RepoStatus(ctx, rsp.Repository)
-		if code.FromError(err) == service.KeyNotFound {
-			repo.Add(rsp.Repository)
-		} else if err != nil {
-			log.Printf("Repo status %q: %v", rsp.Repository, err)
-		}
+		pkg := pkg
+		run(func() error {
+			if seen(pkg) {
+				return nil
+			}
+			rsp, err := c.Resolve(ctx, pkg)
+			if err != nil {
+				log.Printf("Resolving %q: %v", pkg, err)
+				return nil
+			}
+			addPfx(rsp.Prefix)
+			_, err = c.RepoStatus(ctx, rsp.Repository)
+			if code.FromError(err) == service.KeyNotFound {
+				addRepo(rsp.Repository)
+			} else if err != nil {
+				log.Printf("Repo status %q: %v", rsp.Repository, err)
+			}
+			return nil
+		})
+	}
+	if err := g.Wait(); err != nil {
+		log.Fatal(err)
 	}
 	for _, url := range repo.Elements() {
 		fmt.Println(url)
